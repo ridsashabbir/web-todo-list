98d5402321b6c848532abdf5893fa56ae83542ec
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-14 21:08:26 +0500

    contact us page

--- todo-list/src/Components/Contact.js
+++ todo-list/src/Components/Contact.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [submitMessage, setSubmitMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.prevent.default();
+    setSubmitMessage("Message Submitted Successfully!");
+  };
+
   return (
     <div>
       <h1>Contact Us</h1>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" required />
+        <input type="text" placeholder="Subject" required />
+        <textarea
+          name="message"
+          placeholder="Enter your Message"
+          id=""
+          cols="40"
+          rows="7"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{submitMessage}</p>
     </div>
   );
 }
