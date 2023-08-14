09de2d50adba325246102daf22f75e4139e30486
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-14 23:21:36 +0500

    Update Contact.js

--- todo-list/src/Components/Contact.js
+++ todo-list/src/Components/Contact.js
@@ -8,13 +8,49 @@ export default function Contact() {
     setSubmitMessage("Message Submitted Successfully!");
   };
 
+  const myStyle = {
+    border: "1px solid black",
+    width: "50%",
+    padding: "4%",
+    fontSize: "small",
+    color: "white",
+    backgroundColor: "#7A316F",
+    borderRadius: "20px",
+  };
+
+  const myStyleB = {
+    padding: "10px",
+    bottom: "0",
+    marginLeft: "45%",
+    backgroundColor: "#461959",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "1rem",
+  };
+
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className="container my-3" style={myStyle}>
+      <h1 style={{ textAlign: "center" }}>Contact Us</h1>
       <form className="contact-form" onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" required />
-        <input type="text" placeholder="Subject" required />
+        <div className="my-2">
+          <input
+            type="email"
+            placeholder="Email"
+            style={{ width: "70%", marginLeft: "15%", padding: "5px" }}
+            required
+          />
+        </div>
+        <div className="my-2">
+          <input
+            type="text"
+            placeholder="Subject"
+            style={{ width: "70%", marginLeft: "15%", padding: "5px" }}
+            required
+          />
+        </div>
         <textarea
+          style={{ width: "70%", marginLeft: "15%" }}
+          className="my-2"
           name="message"
           placeholder="Enter your Message"
           id=""
@@ -22,7 +58,11 @@ export default function Contact() {
           rows="7"
           required
         />
-        <button type="submit">Submit</button>
+        <div>
+          <button type="submit" style={myStyleB}>
+            Submit
+          </button>
+        </div>
       </form>
       <p>{submitMessage}</p>
     </div>
