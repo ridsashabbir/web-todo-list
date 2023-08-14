d470235dc1094cfc1852dd4844b148a307cb0c8f
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-14 20:42:59 +0500

    added tasks

--- todo-list/src/Components/Todolist.js
+++ todo-list/src/Components/Todolist.js
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 
 export default function Todolist() {
-  const [taskInput, setTaskInput] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   const handleInputChange = (e) => {
     console.log("added task");
     setTaskInput(e.target.value);
   };
 
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    if (taskInput.trim() === "") return;
+    setTasks([...tasks, taskInput]);
+    setTaskInput("");
+  };
+
   return (
     <div>
       <h1>Todo-List</h1>
-      <form>
-        <input type="text" value={taskInput} onChange={handleInputChange} />
+      <form onSubmit={handleTaskSubmit}>
+        <input
+          type="text"
+          value={taskInput}
+          onChange={handleInputChange}
+          required
+        />
+        <button type="submit">Add Task</button>
       </form>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <div key={index}>{task}</div>
+        ))}
+      </ul>
     </div>
   );
 }
