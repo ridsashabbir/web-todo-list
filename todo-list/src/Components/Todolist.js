06925f846b0c0d6fc67b13c6c2e93a43aa7bd862
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-08-14 20:29:49 +0500

    set input field for task input

--- todo-list/src/Components/Todolist.js
+++ todo-list/src/Components/Todolist.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todolist() {
+  const [taskInput, setTaskInput] = useState([]);
+
+  const handleInputChange = (e) => {
+    console.log("added task");
+    setTaskInput(e.target.value);
+  };
+
   return (
     <div>
-      <h1>Todo </h1>
+      <h1>Todo-List</h1>
+      <form>
+        <input type="text" value={taskInput} onChange={handleInputChange} />
+      </form>
     </div>
   );
 }
